65350847a9f93cdc2a265e7d872e037d021a45f1
Mutlu Polatcan <mutlupolatcan@gmail.com>
2018-09-20 13:02:05 +0300

    bug fixes

--- ubuntu/hbase_config_loader.sh
+++ ubuntu/hbase_config_loader.sh
@@ -22,6 +22,7 @@ loadConfig "hbase.zookeeper.property.clientPort" "${HBASE_ZK_CLIENT_PORT}" "hbas
 loadConfig "hbase.client.retries.number" "${HBASE_CLIENT_RETRIES_NUMBER}" "hbase-site.xml"
 loadConfig "zookeeper.recovery.retry" "${HBASE_ZK_RECOVERY_RETRY}" "hbase-site.xml"
 loadConfig "zookeeper.session.timeout" "${HBASE_ZK_SESSION_TIMEOUT}" "hbase-site.xml"
+loadConfig "hbase.procedure.store.wal.use.hsync" "${HBASE_WAL_USE_HSYNC}" "hbase-site.xml"
 loadConfigWithOption "hbase.master" ${HBASE_MASTER_HOSTNAME} "${HBASE_MASTER_HOSTNAME}:${HBASE_MASTER_PORT}" "${HOSTNAME}:${HBASE_MASTER_PORT}" "hbase-site.xml"
 loadConfigWithOption "hbase.zookeeper.quorum" ${HBASE_ZK_QUORUM} "${HBASE_ZK_QUORUM}" "${HOSTNAME}" "hbase-site.xml"
 loadConfigWithOption "hbase.root.dir" ${FS_HOSTNAME} "${FS_DEFAULTFS_PREFIX}${FS_HOSTNAME}:${FS_DEFAULTFS_PORT}${HBASE_ROOT_DIR}" "${FS_DEFAULTFS_PREFIX}${HOSTNAME}:${FS_DEFAULTFS_PORT}${HBASE_ROOT_DIR}" "hbase-site.xml"
