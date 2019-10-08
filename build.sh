e540012a129f67e43be0448bda34643ec4ff586a
Mutlu Polatcan <mutlupolatcan@gmail.com>
2019-10-08 10:30:41 +0300

    alpine versions removed due to lack of glibc

--- build.sh
+++ build.sh
@@ -14,24 +14,27 @@ HBASE_VERSIONS=(
     "1.3.5"
 )
 
-DISTS=(
-    "alpine"
-    "ubuntu"
+JAVA_VERSIONS=(
+    "8"
+    "9"
+    "10"
+    "11"
+    "12"
 )
 
-# $1: DIST
-# $2: HBASE_VERSION
-# $3: HADOOP_VERSION
+# $1: HBASE_VERSION
+# $2: HADOOP_VERSION
+# $3: JAVA_VERSION
 function build_image() {
-    sudo docker build -q -t mpolatcan/hbase:$1-$2-hadoop-$3 --build-arg HBASE_VERSION=$2 --build-arg HADOOP_VERSION=$3 ./$1/
-	sudo docker push mpolatcan/hbase:$1-$2-hadoop-$3
-	sudo docker rmi mpolatcan/hbase:$1-$2-hadoop-$3
+  sudo docker build -q -t mpolatcan/hbase:$1-hadoop-$2-java$3 --build-arg HBASE_VERSION=$1 --build-arg HADOOP_VERSION=$2 --build-arg JAVA_VERSION=$3 ./src/
+	sudo docker push mpolatcan/hbase:$1-hadoop-$2-java$3
+	sudo docker rmi mpolatcan/hbase:$1-hadoop-$2-java$3
 }
 
 for HBASE_VERSION in ${HBASE_VERSIONS[@]}; do
     for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
-        for DIST in ${DISTS[@]}; do
-            build_image $DIST $HBASE_VERSION $HADOOP_VERSION
+        for JAVA_VERSION in ${JAVA_VERSIONS[@]}; do
+            build_image $HBASE_VERSION $HADOOP_VERSION $JAVA_VERSION
         done
     done
 done
