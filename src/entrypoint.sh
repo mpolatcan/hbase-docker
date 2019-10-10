7ae0694822e31555a0e0e9f0eb8cbb1d8ff99f51
Mutlu Polatcan <mutlupolatcan@gmail.com>
2019-10-10 16:23:28 +0300

    dockerfile and entrypoint.sh bug fixes

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,50 +1,40 @@
 #!/usr/bin/env bash
-
-function execCmd() { su - hduser -s /bin/bash -c "$1"; }
-
-function appendToFile() { echo "$1" >> "$2"; }
-
-function startHBaseRegionserver() {
-    if [[ "${HBASE_MANAGES_ZK}" == "true" ]]
-        then
-            appendToFile "export HBASE_MANAGES_ZK=true" "${HBASE_CONF_DIR}/hbase-env.sh"
-            execCmd "${HBASE_HOME}/bin/hbase-daemon.sh start zookeeper"
-    elif [[ "${HBASE_MANAGES_ZK}" == "false" ]]
-        then
-            appendToFile "export HBASE_MANAGES_ZK=false" "${HBASE_CONF_DIR}/hbase-env.sh"
-    fi
-
-    execCmd "${HBASE_HOME}/bin/hbase-daemon.sh start regionserver"
+function start_hbase_regionserver() {
+    hbase-daemon.sh start regionserver
 }
 
-function startHBaseMaster() {
+function start_hbase_master() {
+    hbase-daemon.sh start master
+    hbase-daemon.sh start regionserver
+    hbase-daemon.sh start thrift -p ${HBASE_THRIFT_PORT}
+    hbase-daemon.sh start rest -p ${HBASE_REST_PORT}
+}
+
+function start_hbase_managed_zk() {
     if [[ "${HBASE_MANAGES_ZK}" == "true" ]]
         then
-            appendToFile "export HBASE_MANAGES_ZK=true" "${HBASE_CONF_DIR}/hbase-env.sh"
-            execCmd "${HBASE_HOME}/bin/hbase-daemon.sh start zookeeper"
+            echo "export HBASE_MANAGES_ZK=true" >> "${HBASE_CONF_DIR}/hbase-env.sh"
+            hbase-daemon.sh start zookeeper
     elif [[ "${HBASE_MANAGES_ZK}" == "false" ]]
         then
-            appendToFile "export HBASE_MANAGES_ZK=false" "${HBASE_CONF_DIR}/hbase-env.sh"
+            echo "export HBASE_MANAGES_ZK=false" >> "${HBASE_CONF_DIR}/hbase-env.sh"
     fi
-
-    execCmd "${HBASE_HOME}/bin/hbase-daemon.sh start master"
-    execCmd "${HBASE_HOME}/bin/hbase-daemon.sh start regionserver"
-    execCmd "${HBASE_HOME}/bin/hbase-daemon.sh start thrift -p ${HBASE_THRIFT_PORT}"
-    execCmd "${HBASE_HOME}/bin/hbase-daemon.sh start rest -p ${HBASE_REST_PORT}"
 }
 
 # Start Hadoop services with loaded configurations
 ./hadoop_entrypoint.sh $1
 
 # Load HBase configurations
-/hbase_config_loader.sh
+./hbase_config_loader.sh
 
 # Terminate config load
 echo "</configuration>" >> ${HBASE_CONF_DIR}/hbase-site.xml
 
-[[ "${HBASE_NODE_TYPE}" == "master" ]] && startHBaseMaster
+start_hbase_managed_zk
 
-[[ "${HBASE_NODE_TYPE}" == "regionserver" ]] && startHBaseRegionserver
+[[ "${HBASE_NODE_TYPE}" == "master" ]] && start_hbase_master
+
+[[ "${HBASE_NODE_TYPE}" == "regionserver" ]] && start_hbase_regionserver
 
 if [[ "$1" == "hbase" ]]; then
     tail -f /dev/null
