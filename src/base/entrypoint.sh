32ac52fe11acb5cddee7b389711fb37bff02e5a6
Mutlu Polatcan <mutlupolatcan@gmail.com>
2019-11-24 23:24:16 +0300

    hbase-env.sh important configurations added

--- src/base/entrypoint.sh
+++ src/base/entrypoint.sh
@@ -5,10 +5,27 @@ function start_daemons() {
       # Start current daemon
       hbase-daemon.sh start $daemon
   done
+
+  if [[ "${HBASE_MANAGES_ZK}" == "true" ]]; then
+      hbase-daemon.sh start zookeeper
+  fi
 }
 
-# Load HBase configs
-./hbase_config_loader.sh
+function load_configs() {
+    # Load HBase configs of hbase-site.xml
+    ./hbase_config_loader.sh
+
+    # Load HBase configs of hbase-env.sh
+    echo "export HBASE_MANAGES_ZK=${HBASE_MANAGES_ZK}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
+
+    if [[ "${HBASE_HEAPSIZE}" != "NULL" ]]; then
+      echo "export HBASE_HEAPSIZE=${HBASE_HEAPSIZE}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
+    fi
+
+    if [[ "${HBASE_OFFHEAPSIZE}" != "NULL" ]]; then
+      echo "export HBASE_OFFHEAPSIZE=${HBASE_OFFHEAPSIZE}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
+    fi
+}
 
 # Start Hadoop Daemons
 [[ "${HBASE_DAEMONS}" != "NULL" ]] && start_daemons
