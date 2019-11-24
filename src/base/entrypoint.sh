bf2bff47e7495192bcc341738ed0bc8c9b29e517
Mutlu Polatcan <mutlupolatcan@gmail.com>
2019-11-24 23:49:37 +0300

    hbase-env.sh configurations updated

--- src/base/entrypoint.sh
+++ src/base/entrypoint.sh
@@ -18,12 +18,12 @@ function load_configs() {
     # Load HBase configs of hbase-env.sh
     echo "export HBASE_MANAGES_ZK=${HBASE_MANAGES_ZK}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
 
-    if [[ "${HBASE_HEAPSIZE}" != "NULL" ]]; then
-      echo "export HBASE_HEAPSIZE=${HBASE_HEAPSIZE}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
+    if [[ "${HBASE_HEAP_SIZE}" != "NULL" ]]; then
+      echo "export HBASE_HEAPSIZE=${HBASE_HEAP_SIZE}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
     fi
 
-    if [[ "${HBASE_OFFHEAPSIZE}" != "NULL" ]]; then
-      echo "export HBASE_OFFHEAPSIZE=${HBASE_OFFHEAPSIZE}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
+    if [[ "${HBASE_OFFHEAP_SIZE}" != "NULL" ]]; then
+      echo "export HBASE_OFFHEAPSIZE=${HBASE_OFFHEAP_SIZE}" >> "${HBASE_CONF_DIR}/hbase-env.sh"
     fi
 }
 
